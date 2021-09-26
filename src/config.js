3f6efe291a41025553d966478cb11ea40fea7bc8
Malcolm Kiano <malcolmkiano@gmail.com>
2021-09-26 15:43:08 -0400

    fix: reformat invalid regex

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   CLIENT_ORIGIN: [
-    /^https:\/\/(www\.)?devlft\.com$/,
-    /^https:\/\/dev-lft\.vercel\.app$/,
-    /^https:\/\/dev-lft-.*\.vercel\.app/,
-    /^http:\/\/localhost(:[0-9]+)?$/
+    "https://dev-lft.vercel.app",
+    /dev-lft-.*\.vercel\.app/,
+    /(www\.)?devlft\.com/,
+    /^http:\/\/localhost(:[0-9]+)?$/,
   ],
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
