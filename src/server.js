150005559567dadab22813ea15fb08cf47badfd1
jacklansing <58494268+jacklansing@users.noreply.github.com>
2021-09-26 21:57:28 -0400

    testing for heroku ssl error
    testing for heroku ssl error

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const { PORT, DATABASE_URL } = require('./config');
 // Connect to the database and set it on app
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  }
 });
 
 app.set('db', db);
